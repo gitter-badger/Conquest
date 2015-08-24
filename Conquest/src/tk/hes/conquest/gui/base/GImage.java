0caa2a85f95510e3e26d2499dae86637c1740038
James <16jroberts@gmail.com>
2015-08-24 14:55:43 -0400

    Added GImage for easier bitmap rendering.

---
+++ Conquest/src/tk/hes/conquest/gui/base/GImage.java
@@ -0,0 +1,38 @@
+package tk.hes.conquest.gui.base;
+
+import me.deathjockey.tinypixel.graphics.Bitmap;
+import me.deathjockey.tinypixel.graphics.RenderContext;
+import me.deathjockey.tinypixel.util.Vector2f;
+
+/**
+ * @author James
+ */
+
+public class GImage extends GComponent {
+
+    private Bitmap image;
+
+    public GImage(Bitmap image, Vector2f position) {
+        this(image, position, null);
+
+    }
+
+    public GImage(Bitmap image, Vector2f position, GComponent parent) {
+        super(position, parent);
+        this.image = image;
+    }
+
+    @Override
+    public void init(RenderContext c) {
+    }
+
+    @Override
+    public void render(RenderContext c) {
+        c.render(image, (int) position.getX(), (int) position.getY());
+    }
+
+    @Override
+    public void update() {
+    }
+
+}
